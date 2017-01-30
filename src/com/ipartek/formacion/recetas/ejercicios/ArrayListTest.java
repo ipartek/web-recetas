74727b9bf616faed51ddfc54e85111cd5f88fbd8
anderuraga <ander.uraga@gmail.com>
2017-01-30 12:21:04 +0100

    Interfaz Persistable

--- src/com/ipartek/formacion/recetas/ejercicios/ArrayListTest.java
+++ src/com/ipartek/formacion/recetas/ejercicios/ArrayListTest.java
@@ -1,55 +1,60 @@
 package com.ipartek.formacion.recetas.ejercicios;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.junit.Test;
 
 /**
  * Aprendiendo a usar ArrayList
+ * 
  * @author Ipartek
  *
  */
 public class ArrayListTest {
 
-	ArrayList<String> paises;	
-	
+	ArrayList<String> paises;
+
 	@Test
 	public void test() {
-					
-		assertNull("Deberia ser null",paises);
-		
+
+		assertNull("Deberia ser null", paises);
+
 		paises = new ArrayList<String>();
-		assertNotNull("No deberia ser null",paises);
-		
-		assertEquals("0 paises",0, paises.size());
-			
+		assertNotNull("No deberia ser null", paises);
+
+		assertEquals("0 paises", 0, paises.size());
+
 		paises.add("EH");
-		assertEquals("1 pais", 1 , paises.size());
-		
+		assertEquals("1 pais", 1, paises.size());
+
 		paises.add("kudbejistan");
 		paises.add("portugal");
 		paises.add("Barakaldo");
 		paises.add("Alemania");
-		
-		assertEquals( 5 , paises.size());
-		
-		
+
+		assertEquals(5, paises.size());
+
 		assertEquals("Barakaldo", paises.remove(3));
-		assertEquals( 4 , paises.size());
-		
+		assertEquals(4, paises.size());
+
 		paises.add(1, "Portugalete");
-		assertEquals( 5 , paises.size());		
+		assertEquals(5, paises.size());
 		assertEquals("EH", paises.get(0));
-		assertEquals("Portugalete",paises.get(1));
-		assertEquals("kudbejistan",paises.get(2));
-		
-		
-		
-		
-		
-		
+		assertEquals("Portugalete", paises.get(1));
+		assertEquals("kudbejistan", paises.get(2));
+
+		String pais = "";
+
+		Iterator<String> it = paises.iterator();
+		while (it.hasNext()) {
+			pais = it.next();
+		}
+
 	}
 
 }
