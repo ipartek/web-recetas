002969323e87b6ac796a2a9b7aa60190c452c7fa
nlibano <nagolib@hotmail.com>
2017-02-08 13:29:38 +0100

    UsuarioDAO. pdte terminar metodos delete y exist.

--- src/com/ipartek/formacion/recetas/model/dao/UsuarioDAO.java
+++ src/com/ipartek/formacion/recetas/model/dao/UsuarioDAO.java
@@ -10,6 +10,7 @@ import java.util.List;
 import com.ipartek.formacion.recetas.model.DataBaseConnectionImpl;
 import com.ipartek.formacion.recetas.model.Persistable;
 import com.ipartek.formacion.recetas.pojo.Usuario;
+import com.mysql.jdbc.Statement;
 
 public class UsuarioDAO implements Persistable<Usuario> {
 
@@ -24,7 +25,7 @@ public class UsuarioDAO implements Persistable<Usuario> {
 	static final private String SQL_COUNT = "SELECT COUNT(`id`) FROM `usuario`;";
 	static final private String SQL_CREATE = "INSERT INTO `usuario`(`nombre`, `apellido1`, `apellido2`, `edad`, `email`, `dni`, `puesto`, `password`, `imagen`) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?);";
 	static final private String SQL_DELETE = "DELETE FROM `usuario` WHERE `id` = ?;";
-	static final private String SQL_UPDATE = "UPDATE `usuario` SET `id`= ?,`nombre`= ?,`apellido1`= ?,`apellido2`= ?,`edad`= ?,`email`= ?,`dni`= ?,`puesto`= ?,`password`= ?,`imagen`= ? WHERE `id` = ?;";
+	static final private String SQL_UPDATE = "UPDATE `usuario` SET `nombre`= ?,`apellido1`= ?,`apellido2`= ?,`edad`= ?,`email`= ?,`dni`= ?,`puesto`= ?,`password`= ?,`imagen`= ? WHERE `id` = ?;";
 	static final private String SQL_EXIST = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` WHERE `email` = ? AND `password` = ?;";
 
 	private UsuarioDAO() {
@@ -71,20 +72,117 @@ public class UsuarioDAO implements Persistable<Usuario> {
 
 	@Override
 	public Usuario getById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Usuario u = null;
+
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+
+		try {
+
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_GET_BY_ID);
+			pst.setLong(1, id);
+
+			rs = pst.executeQuery();
+
+			while (rs.next()) {
+				u = mapear(rs);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+			db.desconectar();
+
+		}
+
+		return u;
 	}
 
 	@Override
-	public boolean create(Usuario objeto) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean create(Usuario u) {
+
+		boolean resul = false;
+
+		PreparedStatement pst = null;
+
+		try {
+
+			conn = db.getConexion();
+
+			pst = conn.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS);
+			pst.setString(1, u.getNombre());
+			pst.setString(2, u.getApellido1());
+			pst.setString(3, u.getApellido2());
+			pst.setInt(4, u.getEdad());
+			pst.setString(5, u.getEmail());
+			pst.setString(6, u.getDni());
+			pst.setString(7, u.getPuesto());
+			pst.setString(8, u.getPassword());
+			pst.setString(9, u.getImagen());
+
+			// insertamos un usuario nuevo
+			int affectedRows = pst.executeUpdate();
+
+			if (affectedRows == 1) {
+				// buscamos el ID generado
+				ResultSet generatedKeys = pst.getGeneratedKeys();
+				if (generatedKeys.next()) {
+					u.setId(generatedKeys.getLong(1));
+					resul = true;
+				}
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			db.desconectar();
+
+		}
+
+		return resul;
 	}
 
 	@Override
-	public boolean update(Usuario objeto) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(Usuario u) {
+
+		boolean resul = false;
+		PreparedStatement pst = null;
+
+		try {
+
+			conn = db.getConexion();
+
+			pst = conn.prepareStatement(SQL_UPDATE);
+			pst.setString(1, u.getNombre());
+			pst.setString(2, u.getApellido1());
+			pst.setString(3, u.getApellido2());
+			pst.setInt(4, u.getEdad());
+			pst.setString(5, u.getEmail());
+			pst.setString(6, u.getDni());
+			pst.setString(7, u.getPuesto());
+			pst.setString(8, u.getPassword());
+			pst.setString(9, u.getImagen());
+			pst.setLong(10, u.getId());
+
+			if (pst.executeUpdate() == 1) {
+				resul = true;
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			db.desconectar();
+
+		}
+		return resul;
 	}
 
 	@Override
@@ -94,8 +192,33 @@ public class UsuarioDAO implements Persistable<Usuario> {
 	}
 
 	public Usuario getByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		Usuario u = null;
+
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+
+		try {
+
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_GET_BY_EMAIL);
+			pst.setString(1, email);
+
+			rs = pst.executeQuery();
+
+			while (rs.next()) {
+				u = mapear(rs);
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+			db.desconectar();
+
+		}
+
+		return u;
 	}
 
 	Usuario existe(String email, String password) {
