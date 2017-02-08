beab348648612af3a799d9ac054a775aca729433
nlibano <nagolib@hotmail.com>
2017-02-08 12:53:56 +0100

    UsuarioDAO sin terminar

--- src/com/ipartek/formacion/recetas/model/dao/UsuarioDAO.java
+++ src/com/ipartek/formacion/recetas/model/dao/UsuarioDAO.java
@@ -1,21 +1,72 @@
 package com.ipartek.formacion.recetas.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.ipartek.formacion.recetas.model.DataBaseConnectionImpl;
 import com.ipartek.formacion.recetas.model.Persistable;
 import com.ipartek.formacion.recetas.pojo.Usuario;
 
 public class UsuarioDAO implements Persistable<Usuario> {
 
+	private static UsuarioDAO INSTANCE = null;
+	private static DataBaseConnectionImpl db;
+	private Connection conn;
+
+	// Sentencias SQL
 	static final private String SQL_GET_ALL = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` ORDER BY `id` DESC LIMIT 500;";
 	static final private String SQL_GET_BY_ID = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` WHERE `id` = ?;";
 	static final private String SQL_GET_BY_EMAIL = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` WHERE `email` = ?;";
 	static final private String SQL_COUNT = "SELECT COUNT(`id`) FROM `usuario`;";
+	static final private String SQL_CREATE = "INSERT INTO `usuario`(`nombre`, `apellido1`, `apellido2`, `edad`, `email`, `dni`, `puesto`, `password`, `imagen`) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+	static final private String SQL_DELETE = "DELETE FROM `usuario` WHERE `id` = ?;";
+	static final private String SQL_UPDATE = "UPDATE `usuario` SET `id`= ?,`nombre`= ?,`apellido1`= ?,`apellido2`= ?,`edad`= ?,`email`= ?,`dni`= ?,`puesto`= ?,`password`= ?,`imagen`= ? WHERE `id` = ?;";
+	static final private String SQL_EXIST = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` WHERE `email` = ? AND `password` = ?;";
+
+	private UsuarioDAO() {
+		db = DataBaseConnectionImpl.getInstance();
+	}
+
+	public static UsuarioDAO getInstance() {
+		if (INSTANCE == null) {
+			createInstance();
+		}
+		return INSTANCE;
+	}
+
+	private synchronized static void createInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new UsuarioDAO();
+		}
+	}
 
 	@Override
 	public List<Usuario> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Usuario> list = null;
+
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+
+		try {
+			list = new ArrayList<Usuario>();
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_GET_ALL);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				list.add(mapear(rs));
+			}
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		} finally {
+			db.desconectar();
+		}
+		return list;
 	}
 
 	@Override
@@ -58,6 +109,22 @@ public class UsuarioDAO implements Persistable<Usuario> {
 	 */
 	int count() {
 		return 0;
+	}
+
+	private Usuario mapear(ResultSet rs) throws SQLException {
+		Usuario u = new Usuario();
+		u.setId(rs.getLong("id"));
+		u.setNombre(rs.getString("nombre"));
+		u.setApellido1(rs.getString("apellido1"));
+		u.setApellido2(rs.getString("apellido2"));
+		u.setEdad(rs.getInt("edad"));
+		u.setEmail(rs.getString("email"));
+		u.setDni(rs.getString("dni"));
+		u.setPuesto(rs.String("puesto"));
+		u.setPassword(rs.getString("passwogetrd"));
+		u.setImagen(rs.getString("imagen"));
+
+		return u;
 	}
 
 }
