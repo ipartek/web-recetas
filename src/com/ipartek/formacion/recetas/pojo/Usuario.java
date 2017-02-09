fe1235b75ae0fab2e932734afa8ea60c172a2a6a
markez3030 <emarkez3030@gmail.com>
2017-02-09 09:34:56 +0100

    Cambios pojo usuario

--- src/com/ipartek/formacion/recetas/pojo/Usuario.java
+++ src/com/ipartek/formacion/recetas/pojo/Usuario.java
@@ -4,10 +4,31 @@ public class Usuario {
 
 	private long id;
 	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private int edad;
 	private String email;
+	private String dni;
+	private String puesto;
 	private String password;
 	private String imagen;
-	private String gitUserName;
+
+	
+	
+	public Usuario(long id, String nombre, String apellido1, String apellido2, int edad, String email, String dni,
+			String puesto, String password, String imagen) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.setApellido1(apellido1);
+		this.setApellido2(apellido2);
+		this.setEdad(edad);
+		this.email = email;
+		this.setDni(dni);
+		this.setPuesto(puesto);
+		this.password = password;
+		this.imagen = "https://pbs.twimg.com/profile_images/617295207202209792/P8kGVpP-_reasonably_small.png";
+	}
 
 	public Usuario() {
 		super();
@@ -70,18 +91,57 @@ public class Usuario {
 		this.imagen = imagen;
 	}
 
-	public String getGitUserName() {
-		return gitUserName;
+
+
+
+	public String getApellido1() {
+		return apellido1;
 	}
 
-	public void setGitUserName(String gitUserName) {
-		this.gitUserName = gitUserName;
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
 	}
 
 	@Override
 	public String toString() {
-		return "Usuario [id=" + id + ", nombre=" + nombre + ", email=" + email + ", password=" + password + ", imagen="
-				+ imagen + ", gitUserName=" + gitUserName + "]";
+		return "Usuario [id=" + id + ", nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2=" + apellido2
+				+ ", edad=" + edad + ", email=" + email + ", dni=" + dni + ", puesto=" + puesto + ", password="
+				+ password + ", imagen=" + imagen + "]";
 	}
+	
+	
+	
 
 }
