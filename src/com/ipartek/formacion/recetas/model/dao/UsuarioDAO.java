3525da1262af3ab7bbd5cc8484089d3a6d1dd30e
aloro <alozoyarodriguez@gmail.com>
2017-02-09 08:52:07 +0100

    Primer DAO y capa service

--- src/com/ipartek/formacion/recetas/model/dao/UsuarioDAO.java
+++ src/com/ipartek/formacion/recetas/model/dao/UsuarioDAO.java
@@ -1,54 +1,203 @@
 package com.ipartek.formacion.recetas.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.ipartek.formacion.recetas.model.DataBaseConnectionImpl;
 import com.ipartek.formacion.recetas.model.Persistable;
 import com.ipartek.formacion.recetas.pojo.Usuario;
+import com.mysql.jdbc.Statement;
 
 public class UsuarioDAO implements Persistable<Usuario> {
 
-	static final private String SQL_GET_ALL = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` ORDER BY `id` DESC LIMIT 500;";
-	static final private String SQL_GET_BY_ID = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` WHERE `id` = ?;";
-	static final private String SQL_GET_BY_EMAIL = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` WHERE `email` = ?;";
-	static final private String SQL_COUNT = "SELECT COUNT(`id`) FROM `usuario`;";
+	private static UsuarioDAO INSTANCE = null;
+	private static DataBaseConnectionImpl db;
+	private Connection conn;
+
+	private static final String SQL_GET_ALL = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` ORDER BY `id` DESC LIMIT 500;";
+	private static final String SQL_GET_BY_ID = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` WHERE `id` = ?;";
+	private static final String SQL_GET_BY_EMAIL = "SELECT `id`,`nombre`,`apellido1`,`apellido2`,`edad`,`email`,`dni`,`puesto`,`password`,`imagen` FROM `usuario` WHERE `email` = ?;";
+	private static final String SQL_COUNT = "SELECT COUNT(`id`) FROM `usuario`;";
+	private static final String SQL_UPDATE = "UPDATE `usuario` SET `nombre` = ?, `email` = ?, `imagen` = ? WHERE `id` = ?;";
+	private static String SQL_CREATE = "INSERT INTO `usuario` (`nombre`, `email`, `imagen`) VALUES ( ? , ? , ? );";
+	private static String SQL_DELETE = "DELETE FROM `usuario` WHERE `id` = ?";
+
+	private UsuarioDAO() {
+		db = DataBaseConnectionImpl.getInstance();
+	}
+
+	public static UsuarioDAO getInstance() {
+		if (INSTANCE == null) {
+			createInstance();
+		}
+		return INSTANCE;
+	}
+
+	private synchronized static void createInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new UsuarioDAO();
+		}
+	}
 
 	@Override
 	public List<Usuario> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Usuario> list = null;
+
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+			list = new ArrayList<Usuario>();
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_GET_ALL);
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				list.add(mapear(rs));
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			db.desconectar();
+		}
+		return list;
 	}
 
 	@Override
 	public Usuario getById(long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Usuario u = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_GET_BY_ID);
+			pst.setLong(1, id);
+
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				u = mapear(rs);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.desconectar();
+		}
+		return u;
 	}
 
 	@Override
-	public boolean create(Usuario objeto) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean create(Usuario u) {
+		boolean resul = false;
+		PreparedStatement pst = null;
+		try {
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_CREATE, Statement.RETURN_GENERATED_KEYS);
+			pst.setString(1, u.getNombre());
+			pst.setString(2, u.getEmail());
+			pst.setString(3, u.getImagen());
+
+			// insertamos usuario
+			int affectedRows = pst.executeUpdate();
+
+			if (affectedRows == 1) {
+				// buscamos el ID generado
+				ResultSet generatedKeys = pst.getGeneratedKeys();
+				if (generatedKeys.next()) {
+					u.setId(generatedKeys.getLong(1));
+					resul = true;
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.desconectar();
+		}
+		return resul;
 	}
 
 	@Override
-	public boolean update(Usuario objeto) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean update(Usuario u) {
+		boolean resul = false;
+		PreparedStatement pst = null;
+		try {
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_UPDATE);
+			pst.setString(1, u.getNombre());
+			pst.setString(2, u.getEmail());
+			pst.setString(3, u.getImagen());
+
+			if (pst.executeUpdate() == 1) {
+				resul = true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.desconectar();
+		}
+		return resul;
 	}
 
 	@Override
 	public boolean delete(long id) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean resul = false;
+		PreparedStatement pst = null;
+		try {
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_DELETE);
+			pst.setLong(1, id);
+
+			if (pst.executeUpdate() == 1) {
+				resul = true;
+			}
+
+			pst.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.desconectar();
+		}
+		return resul;
 	}
 
 	public Usuario getByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Usuario u = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_GET_BY_EMAIL);
+			pst.setString(1, email);
+
+			rs = pst.executeQuery();
+			while (rs.next()) {
+				u = mapear(rs);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.desconectar();
+		}
+		return u;
 	}
 
 	Usuario existe(String email, String password) {
-		return null;
+		Usuario resul = null;
+		Usuario u = new Usuario();
+		u = getByEmail(email);
+		if (u.getPassword().equals(password)) {
+			resul = u;
+		}
+		return resul;
 	}
 
 	/**
@@ -57,7 +206,29 @@ public class UsuarioDAO implements Persistable<Usuario> {
 	 * @return <code>int </code>numero entero de registros
 	 */
 	int count() {
-		return 0;
+		int resul = -1;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+			conn = db.getConexion();
+			pst = conn.prepareStatement(SQL_COUNT);
+			rs = pst.executeQuery();
+			resul = rs.getInt(1);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.desconectar();
+		}
+		return resul;
+	}
+
+	private Usuario mapear(ResultSet rs) throws SQLException {
+		Usuario u = new Usuario();
+		u.setNombre(rs.getString("nombre"));
+		u.setEmail(rs.getString("email"));
+		// TODO faltan campos
+		return u;
 	}
 
 }
